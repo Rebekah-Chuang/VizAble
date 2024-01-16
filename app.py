70ef048d4839caeaaaddd6812fda1d6c68880c4f
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-01-16 14:02:38 -0500

    upload file features

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from shiny import ui, render, App
+from shiny import ui, render, App, Inputs, Outputs, Session, reactive
+from shiny.types import FileInfo
+import pandas as pd
+from great_tables import GT
+
 
 app_ui = ui.page_fluid(
-    ui.input_slider("n", "N", 0, 100, 40),
-    ui.output_text_verbatim("txt"),
-)
+    ui.input_file("file1", "Choose CSV File", accept=[".csv"], multiple=False),
+    ui.output_table("output_dataframe")
+    )
 
-def server(input, output, session):
+def server(input: Inputs, output: Outputs, session: Session):
     @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @render.table
 
-# This is a shiny.App object. It must be named `app`.
+    # TODO: not able to render great table in shiny?
+
+    def output_dataframe():
+        file: list[FileInfo] | None = input.file1()
+        if file is None:
+            return pd.DataFrame()
+        return pd.read_csv(file[0]["datapath"])
+        # return GT(pd.read_csv(file[0]["datapath"]))
+
 app = App(app_ui, server)
