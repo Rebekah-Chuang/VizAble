d03897c14492a4a807b7a0200957f838ed3da96f
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-01-17 18:33:52 -0500

    try shiny `@render.dataframe`

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from shiny import ui, render, App, Inputs, Outputs, Session, reactive
-# import htmltools as html
 from shiny.types import FileInfo
+import htmltools
+import shinyswatch
 import pandas as pd
 import great_tables as gt
 import great_tables.shiny as gts
 
 
 app_ui = ui.page_fluid(
-    ui.panel_title("Upload CSV File"),
+    # thwmw for the app
+    shinyswatch.theme.superhero(),
+
+    ui.panel_title("Upload a CSV file",
+                   window_title= "Accessible Data Visualization"),
     ui.input_file(id = "file1",
                   label = "",
                   accept = [".csv", ".tsv", ".txt"],
@@ -25,14 +30,17 @@ app_ui = ui.page_fluid(
         ),
 
         ui.panel_main(
-            gts.output_gt("output_dataframe")
+            # gts.output_gt("output_dataframe")
+            ui.output_data_frame("output_dataframe")
         )
     )
 )
 
 def server(input: Inputs, output: Outputs, session: Session):
+
     @output
-    @gts.render_gt
+    @render.data_frame
+    # @gts.render_gt
 
     def output_dataframe():
         # Seperator
@@ -53,17 +61,25 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         file: list[FileInfo] | None = input.file1()
         if file is None:
-            return gt.GT(pd.DataFrame())
+            # return gt.GT(pd.DataFrame())
+            return render.DataGrid(pd.DataFrame())
         
         data_frame = pd.read_csv(file[0]["datapath"],
                                  sep = sep,
                                  quotechar = quotechar,
-                                 header = 0)
+                                 header = 0).reset_index()
+        
+        return render.DataGrid(data_frame,
+                               filters = True)
+    
+        """ THIS PART IS FOR GREAT TABLES
         return (
-            gt.GT(data = data_frame)
+            gt.GT(data = data_frame,
+                  rowname_col="index")
             .tab_header(title = f"{file[0]['name']}",
-                        subtitle = f"{data_frame.shape[1]} column(s) | {data_frame.shape[0]} row(s)"
-            )
+                        subtitle = f"{data_frame.shape[1]} column(s) | {data_frame.shape[0]} row(s)")
+            .tab_stubhead(label=gt.md("Index"))
         )
+        """
 
 app = App(app_ui, server)
