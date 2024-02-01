7a6dcb45361a67fa0f969a2b2560606d915991b3
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-02-01 16:23:53 -0500

    fix: Fixed the problem where an error occurs when a user uploads a file but hasn't selected a plot type.

--- apps/app.py
+++ apps/app.py
@@ -380,16 +380,15 @@ def server(input: Inputs, output: Outputs, session: Session):
     @reactive.Effect
     @reactive.event(input.file_format, reactive_df, input.sheet_name)
     def update_plot_types_selectize():
+        data_frame = reactive_df.get()
+        if data_frame is not None and not data_frame.empty:
+            choices=["Select an option", "Line Plot", "Bar Plot", "Box Plot", "Histogram", "Scatter Plot"]
+        else:
+            choices=["Select an option"]
+
         ui.update_selectize(
             id="plot_types",
-            choices=[
-                "Select an option",
-                "Line Plot",
-                "Bar Plot",
-                "Box Plot",
-                "Histogram",
-                "Scatter Plot",
-            ],
+            choices=choices,
             selected=None,
         )
 
@@ -541,50 +540,42 @@ def server(input: Inputs, output: Outputs, session: Session):
     @render.data_frame
     def get_output_selected_cols():
         data_frame = reactive_df.get()
+        selected_cols = pd.DataFrame()
+
         if data_frame.empty:
-            return
+            return selected_cols
 
         if input.plot_types() == "Line Plot":
             req(input.line_x_axis(), input.line_y_axis())
             x_col = input.line_x_axis()
             y_col = input.line_y_axis()
             if x_col in data_frame.columns and y_col in data_frame.columns:
-                selected_cols = data_frame[[input.line_x_axis(), input.line_y_axis()]]
-            else:
-                return pd.DataFrame()
+                selected_cols = data_frame[[x_col, y_col]]
 
         elif input.plot_types() == "Bar Plot":
             req(input.bar_x_axis())
             x_col = input.bar_x_axis()
             if x_col in data_frame.columns:
-                selected_cols = data_frame[[input.bar_x_axis()]]
-            else:
-                return pd.DataFrame()
+                selected_cols = data_frame[[x_col]]
 
         elif input.plot_types() == "Box Plot":
             req(input.box_x_axis())
             x_col = input.box_x_axis()
             if x_col in data_frame.columns:
-                selected_cols = data_frame[[input.box_x_axis()]]
-            else:
-                return pd.DataFrame()
+                selected_cols = data_frame[[x_col]]
 
         elif input.plot_types() == "Histogram":
             req(input.hist_x_axis())
             x_col = input.hist_x_axis()
             if x_col in data_frame.columns:
-                selected_cols = data_frame[[input.hist_x_axis()]]
-            else:
-                return pd.DataFrame()
+                selected_cols = data_frame[[x_col]]
 
         elif input.plot_types() == "Scatter Plot":
             req(input.scatter_x_axis(), input.scatter_y_axis())
             x_col = input.scatter_x_axis()
             y_col = input.scatter_y_axis()
             if x_col in data_frame.columns and y_col in data_frame.columns:
-                selected_cols = data_frame[[input.scatter_x_axis(), input.scatter_y_axis()]]
-            else:
-                return pd.DataFrame()
+                selected_cols = data_frame[[x_col, y_col]]
 
         return selected_cols
 
