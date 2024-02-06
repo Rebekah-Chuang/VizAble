1d83b67adeea5035972b4f54091a98c5c3291cfb
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-02-05 20:17:57 -0500

    test: add comment for `test_get_file_id `

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,5 +2,8 @@ import pytest
 import apps.functions as functions
 
 def test_get_file_id():
+    """
+    Test the `get_file_id()` function. This function generates a `file_id` based on the file format selected by the user. For example, if the user selects ".csv", the returned `file_id` will be "csv_file". Similarly, if ".xlsx" is selected, the returned `file_id` will be "xlsx_file".
+    """
     result = functions.get_file_id(".csv")
     assert result == "csv_file"
