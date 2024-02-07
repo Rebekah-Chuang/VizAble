d4327f3716b3b947d7067de4b6c5d972e3bf1c3d
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-02-06 21:36:24 -0500

    test: update `test_get_file_id()` in `tests/test_functions.py`

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,5 +5,6 @@ def test_get_file_id():
     """
     Test the `get_file_id()` function. This function generates a `file_id` based on the file format selected by the user. For example, if the user selects ".csv", the returned `file_id` will be "csv_file". Similarly, if ".xlsx" is selected, the returned `file_id` will be "xlsx_file".
     """
-    result = functions.get_file_id(".csv")
-    assert result == "csv_file"
+    assert functions.get_file_id(".csv") == "csv_file"
+    assert functions.get_file_id(".tsv") == "tsv_file"
+    assert functions.get_file_id(".xlsx") == "xlsx_file"
