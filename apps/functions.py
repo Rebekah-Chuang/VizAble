1b4900ab47b891e64fa2b0cb19fd373b7c8908dc
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-02-12 21:13:35 -0500

    docs: modify the docstring in functions.py by using the autoDocstring - Python Docstring Generator extension

--- apps/functions.py
+++ apps/functions.py
@@ -4,8 +4,11 @@ from shiny import ui, render, App, Inputs, Outputs, Session, reactive
 import openpyxl
 
 def sep_input_radio_buttons() -> ui.input_radio_buttons:
-    """
-    Create a radio button for user to select separator as input
+    """ Create a radio button group for users to select a separator for input.
+
+    Returns:
+        ui.input_radio_buttons: A radio button group for selecting a separator.
+            Users can choose from Comma (`,`), Semicolon (`;`), and Tab (`\t`).
     """
     return ui.input_radio_buttons(
         id = "sep",
@@ -15,8 +18,11 @@ def sep_input_radio_buttons() -> ui.input_radio_buttons:
     )
 
 def quotechar_input_radio_buttons() -> ui.input_radio_buttons:
-    """
-    Create a radio button for user to select quote character as input
+    """ Create a radio button group for users to select a quote character for input.
+
+    Returns:
+        ui.input_radio_buttons: A radio button group for selecting a quote character.
+            Users can choose from Double Quote (`"`) and Single Quote (`'`).
     """
     return ui.input_radio_buttons(
         id = "quotechar",
@@ -26,8 +32,13 @@ def quotechar_input_radio_buttons() -> ui.input_radio_buttons:
     )
 
 def input_file(file_extension_str: str) -> ui.input_file:
-    """
-    Create a file input for user to upload file
+    """ Create a file input for users to upload a file.
+
+    Args:
+        file_extension_str (str): File extension string, e.g., ".csv", ".tsv", ".xlsx".
+
+    Returns:
+        ui.input_file: File input for users to upload a file, with the input id based on the provided `file_extension_str`.
     """
     file_suffix = file_extension_str.replace(".", "")
 
@@ -39,16 +50,26 @@ def input_file(file_extension_str: str) -> ui.input_file:
     )
 
 def get_file_id(input_file_format_str: str) -> str:
-    """
-    Generate `file_id` based on file format user selected
+    """ Generate a `file_id` based on the file format selected by the user.
+
+    Args:
+        input_file_format_str (str): File format string, e.g., ".csv", ".tsv", ".xlsx".
+
+    Returns:
+        str: The `file_id` based on the provided file format string.
     """
     file_suffix = input_file_format_str.replace(".", "")
     file_id = f"{file_suffix}_file"
     return file_id
 
 def xaxis_input_select(plot_type_str: str) -> ui.input_select:
-    """
-    Create a dropdown for user to select x-axis
+    """ Create a dropdown for users to select the x-axis.
+
+    Args:
+        plot_type_str (str): Plot type string, e.g., "line", "bar", "box", "histogram", "scatter".
+
+    Returns:
+        ui.input_select: An empty dropdown for users to select the `x-axis`, where the input id is based on the provided `plot_type_str`.
     """
     x_axis_id = f"{plot_type_str}_x_axis"
     return ui.input_select(
@@ -60,8 +81,13 @@ def xaxis_input_select(plot_type_str: str) -> ui.input_select:
     )
 
 def yaxis_input_select(plot_type_str: str) -> ui.input_select:
-    """
-    Create a dropdown for user to select y-axis
+    """ Create a dropdown for users to select the y-axis.
+
+    Args:
+        plot_type_str (str): Plot type string, e.g., "line", "bar", "box", "histogram", "scatter".
+
+    Returns:
+        ui.input_select: An empty dropdown for users to select the `y-axis`, where the input id is based on the provided `plot_type_str`.
     """
     y_axis_id = f"{plot_type_str}_y_axis"
     return ui.input_select(
@@ -73,6 +99,15 @@ def yaxis_input_select(plot_type_str: str) -> ui.input_select:
     )
 
 def get_excel_sheet_names(file_path: Path) -> List[str]:
+    """ Get a list of all sheet names for the uploaded Excel file.
+
+    Args:
+        file_path (Path): Path to the uploaded Excel file.
+
+    Returns:
+        List[str]: A list of all sheet names for the uploaded Excel file. 
+            Returns an empty list if the file path is invalid or there is an error reading the file.
+    """
     try:
         workbook = openpyxl.load_workbook(file_path, read_only=True)
         sheet_names = workbook.sheetnames
