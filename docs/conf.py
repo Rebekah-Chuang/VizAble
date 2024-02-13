734a2e3afd2f5a9e7a60952395eca36193bf0624
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-02-13 16:46:46 -0500

    chore: setup for sphinx before executing `make html`

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup------------------------------ --------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Accessible Data Visualization Application'
+copyright = '2024, Rebekah Chuang'
+author = 'Rebekah Chuang'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# avalable themes: https://sphinx-themes.org/
+# press, sphinx_material, alabaster, sphinx_wagtail_theme
+html_theme = 'press'
+html_static_path = ['_static']
