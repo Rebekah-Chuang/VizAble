3c67a617e14cb98e642bfee89bc0bca25263fb9b
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-02-14 19:01:30 -0500

    fix: address the issue where it's not possible for `ui.input_select`and `ui.input_radio_buttons` with no items selected

--- apps/app.py
+++ apps/app.py
@@ -1,4 +1,4 @@
-from . import functions
+from apps import functions
 import openpyxl
 import pandas as pd
 import shinyswatch
@@ -38,8 +38,9 @@ app_ui = ui.page_navbar(
                     ui.input_select(
                         id="file_format",
                         label=ui.strong("File Format"),
-                        choices=[".csv", ".tsv", ".xlsx"],
+                        choices=["", ".csv", ".tsv", ".xlsx"],
                         width="100%",
+                        selected=[],
                     ),
                     # Add condition: if user selects ".csv" on file_format, they need to select separator/quote character
                     # and only allowed to upload .csv file
