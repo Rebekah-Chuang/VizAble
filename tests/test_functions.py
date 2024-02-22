45aea21e561eff11b79763ff54f1e5d23746dc1a
Chi-Ying Chuang <83235098+Rebekah-Chuang@users.noreply.github.com>
2024-02-22 14:28:58 -0500

    test: add unit test for `return_choices_for_columns()` function
    This commit adds unit tests for the `return_choices_for_columns()` function to verify that it returns a list of column names with the appropriate data type when a user selects a specific plot type. For instance, a line plot should return all column names; a bar plot should return only columns with categorical data types; and box plots, histograms, and scatter plots should return only columns with numerical data types.
    
    `@pytest.mark.parametrize is also used in this unit test to test with different plot types, thereby avoiding repetition.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,6 @@
 import pytest
 import apps.functions as functions
+import pandas as pd
 
 def test_get_file_id():
     """
@@ -20,3 +21,24 @@ def test_get_excel_sheet_names_invalid():
     Test the `get_excel_sheet_names()` function. This function returns an empty list if the file path is invalid or there is an error reading the file.
     """
     assert functions.get_excel_sheet_names("tests/test_dataframe/invalid_file.xlsx") == []
+
+@pytest.mark.parametrize("plot_type,expected_columns",
+                         [
+                             ("Line Plot", ["Select an option", "Entity", "Code", "Year", "Users"]),
+                             ("Bar Plot", ["Select an option", "Entity", "Code"]),
+                             ("Box Plot", ["Select an option", "Year", "Users"]),
+                             ("Histogram", ["Select an option", "Year", "Users"]),
+                             ("Scatter Plot", ["Select an option", "Year", "Users"])
+                         ])
+
+def test_return_choices_for_columns(plot_type, expected_columns):
+    """ Test the `return_choices_for_columns()` function. This function returns the expected columns based on the plot type selected by the user.
+
+    :param plot_type: the plot type passed to the function
+    :type plot_type: str
+    :param expected_columns: the expected columns returned by the function
+    :type expected_columns: List[str]
+    """
+    data_frame = pd.read_csv("tests/test_dataframe/csv_comma_no_quote.csv")
+    columns = functions.return_choices_for_columns(data_frame, plot_type=plot_type)
+    assert columns == expected_columns
